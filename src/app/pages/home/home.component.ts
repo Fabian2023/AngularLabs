7eb0f8333bf7f7a636c78c321154da1255dce693
Fabian2023 <fasalpe34@gmail.com>
2024-12-20 16:53:52 -0500

    feat(exodus): :sparkles: todolist add and delete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { NgFor } from '@angular/common';
 
+import{Task} from "../../models/task.model"
+
 @Component({
   selector: 'app-home',
   imports: [NgFor],
@@ -8,15 +10,34 @@ import { NgFor } from '@angular/common';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal([
-    'instalar el angular cli',
-    'crear proyecto',
-    'crear componentes',
+  tasks = signal<Task[]>([
+    {
+      id:Date.now(),
+      title:"prubaMaxima",
+      completed: false
+    },
+    {
+      id:Date.now(),
+      title:"prubaMaxima2",
+      completed: false
+    }
+    
   ]);
   //agregar tareas
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask)
+    
+  }
+
+  addTask(title:string){
+    const newTask ={
+      id: Date.now(),
+      title,
+      completed:false
+
+    }
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
   //eliminar tareas
