f9f75f838f15ed310229d02dc60f8e2bbf67c474
Fabian2023 <fasalpe34@gmail.com>
2025-01-10 11:37:04 -0500

    feat(exodus): :zap: Forms validators

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,49 +1,57 @@
 import { Component, signal } from '@angular/core';
-import { NgFor,CommonModule,NgIf } from '@angular/common';
+import { NgFor, CommonModule, NgIf } from '@angular/common';
+import { ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
 
-import{Task} from "../../models/task.model"
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
-  imports: [NgFor,CommonModule,NgIf],
+  imports: [NgFor, CommonModule, NgIf, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   tasks = signal<Task[]>([
     {
-      id:Date.now(),
-      title:"Instalar Angular CLI",
-      completed: false
+      id: Date.now(),
+      title: 'Instalar Angular CLI',
+      completed: false,
     },
     {
-      id:Date.now(),
-      title:"Crear Proyecto",
-      completed: false
+      id: Date.now(),
+      title: 'Crear Proyecto',
+      completed: false,
     },
     {
-      id:Date.now(),
-      title:"Crear Componente",
-      completed: false
-    }
-    
+      id: Date.now(),
+      title: 'Crear Componente',
+      completed: false,
+    },
   ]);
-  addTask(title:string){
-    const newTask ={
+  addTask(title: string) {
+    const newTask = {
       id: Date.now(),
       title,
-      completed:false
-
-    }
+      completed: false,
+    };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+
   //agregar tareas
-  changeHandler(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-    this.addTask(newTask)
-    input.value = '';
-    
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if(value !== ''){
+
+        this.addTask(value);
+        this.newTaskCtrl.setValue('')
+      }
+    }
   }
 
   //eliminar tareas
@@ -55,7 +63,9 @@ export class HomeComponent {
   //tarea completada
   completeTask(index: number) {
     this.tasks.update((tasks) =>
-      tasks.map((task, position) => position === index ? { ...task, completed: true } : task )
-  );
+      tasks.map((task, position) =>
+        position === index ? { ...task, completed: true } : task
+      )
+    );
   }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,39 +1,40 @@
 import { Component, signal } from '@angular/core';
-import { NgFor,NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
-  imports: [NgFor,NgIf],
+  imports: [NgFor, NgIf,ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   welcome = 'Bienvenidos al curso de front';
-  tasks =signal(  ['instalar el angular cli', 
-           'crear proyecto', 
-           'crear componentes'
-          ])
+  tasks = signal([
+    'instalar el angular cli',
+    'crear proyecto',
+    'crear componentes',
+  ]);
   person = signal({
     name: 'Santiago',
     edad: 19,
-    avatar:"https://w3schools.com/howto/img_avatar.png"
-  })
+    avatar: 'https://w3schools.com/howto/img_avatar.png',
+  });
 
-  clickHandler (){
-    alert("hola perroskys")
+  colorCtrl = new FormControl()
+
+  clickHandler() {
+    alert('hola perroskys');
   }
 
   changeAge(event: Event) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
-    this.person.update(prevState =>{
-      return{
+    this.person.update((prevState) => {
+      return {
         ...prevState,
-        edad: parseInt(newValue,10)
-      }
-    })
-  
-    
+        edad: parseInt(newValue, 10),
+      };
+    });
   }
-
 }
